0b314238955fbe0a91308c25840e68bb1e6431d5
conrad <whalliburton@gmail.com>
2016-01-17 14:44:40 -0700

    animation string at end to show complete

--- init/last.el
+++ init/last.el
@@ -1,8 +1,10 @@
 ;; last.el
 
+(use-package animate)
+
+(animate-string ";; Emacs initialization complate." 0 0)
+
 ;; Turn gc back on
 (setq gc-cons-threshold 400000)
 
 ;; (view-buffer "*Messages*")
-
-
