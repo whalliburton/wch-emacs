1253e756d7fcb9fcbdc5e5852bfaefab67588e33
conrad <whalliburton@gmail.com>
2016-01-16 19:02:33 -0700

    mail fix

--- init/mail.el
+++ init/mail.el
@@ -181,10 +181,10 @@
 ;; the 'All Mail' folder by pressing ``ma''.
 
 (setq mu4e-maildir-shortcuts
-      '( ("/INBOX"               . ?i)
-         ("/[Gmail].Sent Mail"   . ?s)
-         ("/[Gmail].Trash"       . ?t)
-         ("/[Gmail].All Mail"    . ?a)))
+      '( ("/whalliburton/INBOX"               . ?i)
+         ("/whalliburton/[Gmail].Sent Mail"   . ?s)
+         ("/whalliburton/[Gmail].Trash"       . ?t)
+         ("/whalliburton/[Gmail].All Mail"    . ?a)))
 
 ;; allow for updating mail using 'U' in the main view:
 (setq mu4e-get-mail-command "offlineimap")
