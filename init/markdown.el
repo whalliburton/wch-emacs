894d49d65881f47e02561d579a06a9f5ade4da06
conrad <whalliburton@gmail.com>
2016-01-19 11:03:11 -0700

    markdown mode

---
+++ init/markdown.el
@@ -0,0 +1,6 @@
+;; markdown.el
+
+(use-package markdown-mode)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
