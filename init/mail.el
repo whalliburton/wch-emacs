5326a39ab4f1551ee4e9a7fefced4647e0ef29b2
conrad <whalliburton@gmail.com>
2016-03-08 11:12:37 -0700

    *default-mail-account*

--- init/mail.el
+++ init/mail.el
@@ -88,18 +88,21 @@
       (mapcar (lambda (account) (cadr (assq 'user-mail-address account)))
                             my-mu4e-account-alist))
 
+(defvar *default-mail-account* "will")
+
 (defun my-mu4e-set-account ()
   "Set the account for composing a message."
   (let* ((account
-          (if mu4e-compose-parent-message
-              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
-                (string-match "/\\(.*?\\)/" maildir)
-                (match-string 1 maildir))
-            (completing-read (format "Compose with account: (%s) "
-                                     (mapconcat #'(lambda (var) (car var))
-                                                my-mu4e-account-alist "/"))
-                             (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
-                             nil t nil nil (caar my-mu4e-account-alist))))
+          (or *default-mail-account*
+              (if mu4e-compose-parent-message
+                  (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                    (string-match "/\\(.*?\\)/" maildir)
+                    (match-string 1 maildir))
+                (completing-read (format "Compose with account: (%s) "
+                                         (mapconcat #'(lambda (var) (car var))
+                                                    my-mu4e-account-alist "/"))
+                                 (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+                                 nil t nil nil (caar my-mu4e-account-alist)))))
          (account-vars (cdr (assoc account my-mu4e-account-alist))))
     (if account-vars
         (mapc #'(lambda (var)
